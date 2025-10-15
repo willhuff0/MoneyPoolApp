fd1d3621dae5f2285fc0026a82093a6b98fd2905
qingyuan <qingyuantian2006@gmail.com>
2025-10-15 14:47:58 -0400

    models

---
+++ server/src/models.ts
@@ -0,0 +1,39 @@
+import { Schema, model, Types } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        user_id : {type: Number, required: true, unique: true},
+        name : {type: String, required: true},
+        chomp_score : {type: Number, required: true}
+
+    },
+    {timestamps: true}
+);
+
+export const User = model("User", userSchema);
+
+
+const sessionTokenSchema = new Schema(
+  {
+    user_id: { type: Number, required: true, unique: true },
+    name: { type: String, required: true }, 
+    time: { type: Date, default: Date.now },  
+    ip: { type: String, required: false },
+    claims: { type: Object, default: {} }, 
+    signature: { type: String, required: true }
+  },
+  { timestamps: true }
+);
+
+export const SessionToken = model("SessionToken", sessionTokenSchema);
+
+const groupSchema = new Schema(
+    {
+
+        group_id: {type : Number, required: true, unique:true},
+        name: {type: String, required: true},
+        owner: { type: Types.ObjectId, ref: "User", required: true },
+        members: [{ type: Types.ObjectId, ref: "User" }],
+        balance: {type : Number, required: true}
+    }
+);

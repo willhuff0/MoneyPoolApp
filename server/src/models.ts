c076fbd7d18262907a2244490823868f1e4c38a3
qingyuan <qingyuantian2006@gmail.com>
2025-10-22 14:04:26 -0400

    finished models

--- server/src/models.ts
+++ server/src/models.ts
@@ -43,8 +43,8 @@ export const Group = model("Group", groupSchema);
 const transactionSchema = new Schema(
   {
     transaction_id: {type: Number, required: true, unique:true},
-    group_id: {type: Number, required:true, unique:true},
-    user_id: {type: Number, required:true, unique:true},
+    group_id: {type: Types.ObjectId, ref: "Group", required:true},
+    user_id: {type: Types.ObjectId, ref: "User", required:true},
     amount: {type: Number, required:true},
     description: {type: String, required: true}
   },

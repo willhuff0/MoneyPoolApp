b55599608461758bd88598e1df172a72dadeb565
willhuff0 <wehuffman1@gmail.com>
2025-10-27 11:13:26 -0400

    Ignored build folders

--- shared/dist/src/validation.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const validateDisplayName: (displayName: string) => boolean;
-export declare const validateUserName: (userName: string) => boolean;
-export declare const validateEmail: (email: string) => boolean;
-export declare const validatePassword: (password: string) => boolean;
-//# sourceMappingURL=validation.d.ts.map

62bc1161d481cb0f09c98d7a974b8f07699347fe
willhuff0 <wehuffman1@gmail.com>
2025-11-13 17:03:54 -0500

    Pool Sdk

---
+++ client/api/endpoints/pool.ts
@@ -0,0 +1,45 @@
+import * as Protocol from "@money-pool-app/shared";
+import { AxiosInstance } from "axios";
+
+export const getPool = (client: AxiosInstance) => async (poolId: string): Promise<Protocol.Pool | null> => {
+    const response = await client.post(Protocol.poolGetPoolEndpoint, {
+        poolId: poolId,
+    } as Protocol.PoolGetPoolRequest);
+    if (response.status !== 200) return null;
+
+    const body = response.data as Protocol.PoolGetPoolResponse;
+    return body.pool;
+}
+
+export const createPool = (client: AxiosInstance) => async (name: string): Promise<string | null> => {
+    const response = await client.post(Protocol.poolCreatePoolEndpoint, {
+        name: name,
+    } as Protocol.PoolCreatePoolRequest);
+    if (response.status !== 200) return null;
+
+    const body = response.data as Protocol.PoolCreatePoolResponse;
+    return body.poolId;
+}
+
+export const deletePool = (client: AxiosInstance) => async (poolId: string): Promise<boolean> => {
+    const response = await client.post(Protocol.poolDeletePoolEndpoint, {
+        poolId: poolId,
+    } as Protocol.PoolDeletePoolRequest);
+    return response.status === 200;
+}
+
+export const addMember = (client: AxiosInstance) => async (poolId: string, userId: string): Promise<boolean> => {
+    const response = await client.post(Protocol.poolAddMemberEndpoint, {
+        poolId: poolId,
+        userId: userId,
+    } as Protocol.PoolAddMemberRequest);
+    return response.status === 200;
+}
+
+export const removeMember = (client: AxiosInstance) => async (poolId: string, userId: string): Promise<boolean> => {
+    const response = await client.post(Protocol.poolRemoveMemberEndpoint, {
+        poolId: poolId,
+        userId: userId,
+    } as Protocol.PoolRemoveMemberRequest);
+    return response.status === 200;
+}

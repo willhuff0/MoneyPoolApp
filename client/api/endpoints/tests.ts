67a3cb49a9e365108724d56d622dac47eba5d513
willhuff0 <wehuffman1@gmail.com>
2025-11-13 14:33:46 -0500

    Added client sdk for friend requests

--- client/api/endpoints/tests.ts
+++
@@ -1,8 +0,0 @@
-import { AxiosInstance } from "axios";
-
-export const test = (client: AxiosInstance) => async (param: number) => {
-    const response = await client.post('test', {
-        param: param + 1,
-    });
-    return response.data;
-}
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -9,6 +9,7 @@ export const getUserRouter = (userDao: UserDao): Router => {
     const controller = new UserController(userDao);
 
     router.all('/getUser', controller.getUser);
+    router.all('/searchUser', controller.searchUser);
     router.all('/createFriendRequest', controller.createFriendRequest);
     router.all('/deleteFriendRequest', controller.deleteFriendRequest);
     router.all('/acceptFriendRequest', controller.acceptFriendRequest);
